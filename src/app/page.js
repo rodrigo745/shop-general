92231e4c9c8e5e795aa975596021749775e0a0e6
roodrigoquinteros <roodrigoquinteros@gmail.com>
2024-06-12 18:52:38 +0000

    inicio

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
-import Image from "next/image";
+// Componentes
+import Nav from "@/components/Nav";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Nav / >
       
 
     </main>
