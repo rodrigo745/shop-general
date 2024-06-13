81f80e95f53d0135bf44a9bb5de1d2132bca7eb6
roodrigoquinteros <roodrigoquinteros@gmail.com>
2024-06-13 11:20:26 +0000

    form

--- src/app/pages/control/agregarDestacado/page.jsx
+++ src/app/pages/control/agregarDestacado/page.jsx
@@ -3,7 +3,7 @@ export default function ProductoDestacado(){
         <div className="md:mt-5">
             <h3 className="font-bold md:text-xl">Agregar producto destacado</h3>
             <div className="flex flex-col md:flex md:flex-row justify-between md:space-x-16 mt-4">
-                <div className="md:w-[40%] xl:w-[30%] space-y-3">
+                <div className="md:w-[40%] xl:w-[30%] space-y-5">
                     <input type="text" placeholder="Nombre" className={estiloInput} />
                     <textarea className={`${estiloInput} resize-none h-32`} placeholder="Descripción" />
                     <input type="text" placeholder="Precio" className={estiloInput} />
@@ -18,8 +18,8 @@ export default function ProductoDestacado(){
                     </div>
                     <input type="text" placeholder="Productos relacionados" className={estiloInput} />
                     <input type="text" placeholder="Cargar imagen principal" className={estiloInput} />
-                    <input type="text" placeholder="Cargar imagenes secundaria" className={estiloInput} />
-                    <button className={estiloInput}>Agregar</button>
+                    <input type="text" placeholder="Cargar imagenes secundarias" className={estiloInput} />
+                    <button className="fondoCli p-2 text-white font-bold w-full border-2 rounded-lg">Agregar</button>
                 </div>
                 <div className="md:w-[60%] xl:w-[70%] mt-10 md:mt-0">B</div>
             </div>
@@ -27,4 +27,4 @@ export default function ProductoDestacado(){
     )
 }
 
-const estiloInput = "fondoCli2 p-1.5 px-2 placeholder:text-white text-white w-full border-2 rounded-lg";
+const estiloInput = "fondoCli2Input p-2 px-3 placeholder:text-white text-white w-full border-2 rounded-lg";
