97209ca0de6da01591e53ffc53439b9e32a622aa
Rodrigo745 <roodrigoquinteros@gmail.com>
2024-06-16 10:15:31 -0300

    eliminar colores

--- src/components/admin/destacado/Colores.jsx
+++ src/components/admin/destacado/Colores.jsx
@@ -14,6 +14,11 @@ export default function Colores(props){
         });
     }
 
+    const eliminar = (e)=>{
+        const seleccion = e.target.id;
+        console.log(seleccion);
+    }
+
     const getColor = (e)=>{setColor(e.target.value)}
 
 
@@ -28,15 +33,15 @@ export default function Colores(props){
                     <div className="px-5 mt-3 space-y-4">
                         <p>Selecciona y agrega los colores disponibles</p>
                         <input onChange={getColor} type="color" className="h-8 mr-3 p-1" />
-                        <input type="text" value={color ? color : "#000000"} className="h-8 px-3 rounded-md bg-slate-300"/>
+                        <input type="text" value={color ? color : "#000000"} className="h-8 px-3 rounded-md bg-slate-300" />
                         <button onClick={agregar} className="bg-slate-700 p-1 ml-3 px-4 text-white rounded-md">Agregar</button>
                         <p>Lista de colores seleccionados</p>
                         <div className="flex space-x-5">
                             {
                                 listaColores.map((e, index)=> (
                                     <div key={index} className="flex flex-col items-center justify-center border p-2 rounded-md hover:bg-slate-200 cursor-pointer relative">
-                                        <div className="opacity-0 hover:opacity-100 transition absolute bg-red-700 text-white w-full h-full text-center pt-6 rounded-md">Eliminar</div>
-                                        <div className={`rounded-full mb-2 w-8 h-8 bg-[${e}] shadow-md`}></div>
+                                        <div onClick={eliminar} id={index} className="opacity-0 hover:opacity-100 transition absolute bg-red-700 text-white w-full h-full text-center pt-6 rounded-md">Eliminar</div>
+                                        <div className={`rounded-full mb-2 w-8 h-8 shadow-md`} style={{backgroundColor: e}}></div>
                                         <p>{e}</p>
                                     </div>
                                 ))
