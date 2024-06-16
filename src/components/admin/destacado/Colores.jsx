aca9d85f6dc20dcf6ba3071969684152ed742fbd
Rodrigo745 <roodrigoquinteros@gmail.com>
2024-06-16 00:25:50 -0300

    a

--- src/components/admin/destacado/Colores.jsx
+++ src/components/admin/destacado/Colores.jsx
@@ -4,7 +4,15 @@ import { useState } from "react"
 export default function Colores(props){
 
     const [ color, setColor ] = useState();
-    const [ listaColores, setListaColores ] = useState();
+    const [ listaColores, setListaColores ] = useState([]);
+
+    const agregar = ()=>{
+        setListaColores((prevLista)=>{
+            if(color !== undefined ){
+                return [...prevLista, color];
+            }
+        });
+    }
 
     const getColor = (e)=>{setColor(e.target.value)}
 
@@ -19,21 +27,20 @@ export default function Colores(props){
                     {/* Contenido */}
                     <div className="px-5 mt-3 space-y-4">
                         <p>Selecciona y agrega los colores disponibles</p>
-                        <input type="color" className="h-8 mr-3 p-1" />
-                        <input type="text" value={color ? color : "#000000"} className="h-8 rounded-md bg-slate-300"/>
-                        <button className="bg-slate-700 p-1 ml-3 px-4 text-white rounded-md">Agregar</button>
+                        <input onChange={getColor} type="color" className="h-8 mr-3 p-1" />
+                        <input type="text" value={color ? color : "#000000"} className="h-8 px-3 rounded-md bg-slate-300"/>
+                        <button onClick={agregar} className="bg-slate-700 p-1 ml-3 px-4 text-white rounded-md">Agregar</button>
                         <p>Lista de colores seleccionados</p>
                         <div className="flex space-x-5">
-                            <div className="flex flex-col items-center justify-center border p-2 rounded-md hover:bg-slate-200 cursor-pointer relative">
-                                <div className="opacity-0 hover:opacity-100 transition absolute bg-red-700 text-white w-full h-full text-center pt-6 rounded-md">Eliminar</div>
-                                <div className={`rounded-full w-8 h-8 bg-cyan-700 shadow-md`}></div>
-                                <p>#0891b2</p>
-                            </div>
-                            <div className="flex flex-col items-center justify-center border p-2 rounded-md hover:bg-slate-200 cursor-pointer relative">
-                                <div className="opacity-0 hover:opacity-100 transition absolute bg-red-700 text-white w-full h-full text-center pt-6 rounded-md">Eliminar</div>
-                                <div className={`rounded-full w-8 h-8 bg-cyan-700 shadow-md`}></div>
-                                <p>#0891b2</p>
-                            </div>
+                            {
+                                listaColores.map((e, index)=> (
+                                    <div key={index} className="flex flex-col items-center justify-center border p-2 rounded-md hover:bg-slate-200 cursor-pointer relative">
+                                        <div className="opacity-0 hover:opacity-100 transition absolute bg-red-700 text-white w-full h-full text-center pt-6 rounded-md">Eliminar</div>
+                                        <div className={`rounded-full mb-2 w-8 h-8 bg-[${e}] shadow-md`}></div>
+                                        <p>{e}</p>
+                                    </div>
+                                ))
+                            }
                         </div>
                     </div>
                 </div>
