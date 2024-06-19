4e3bfa56db15eb078ce8a8b2bd36693265f94f3d
Rodrigo745 <roodrigoquinteros@gmail.com>
2024-06-19 13:34:04 -0300

    back

--- src/app/api/destacadosImg/route.js
+++ src/app/api/destacadosImg/route.js
@@ -1,6 +1,26 @@
 import { NextResponse } from "next/server";
+import { v2 as cloudinary } from 'cloudinary';
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: 'diu0ksiyr', 
+    api_key: process.env.CLOUDINARY_KEY, 
+    api_secret: process.env.CLOUDINARY_SECRET // Click 'View Credentials' below to copy your API secret
+});
 
 
+export async function POST(request){
+    const data = await request.formData();
+    const image = !data.get("file");
+
+    if(image){
+        return NextResponse.json("No se ha subido ninguna imagen", { status: 404});
+    } 
+    const bytes = image.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    return NextResponse.json("imagen subida");
+}
 
 
 
--- src/components/admin/destacado/AgregarImagen.jsx
+++ src/components/admin/destacado/AgregarImagen.jsx
@@ -1,15 +1,23 @@
 "use client";
+import { useState } from "react";
 
 export default function AgregarImagen({estilo}){
+    
+    const [ file, setFile ] = useState(null);
+    
     return(
         <form className={`${estilo} flex justify-between`}
             onSubmit={async(e)=> {
                 e.preventDefault();
-                const response = await fetch("api/destacadosImg", { method: "POST" })
+
+                const formData = new FormData();
+                formData.append("file", file);
+                
+                const response = await fetch("/api/destacadosImg", { method: "POST", body: formData })
                 const data = await response.json();
                 console.log(data)
             }}>
-            <input type="file" onChange={(e)=> {console.log(e.target.files[0])}} />
+            <input type="file" onChange={(e)=> {setFile(e.target.files[0])}} />
             <button className="bg-neutral-100 text-black px-4 font-bold h-7 rounded-md">Subir</button>
         </form>
     )
